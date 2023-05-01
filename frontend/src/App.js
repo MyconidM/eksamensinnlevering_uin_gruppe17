ecd3d8cfd2ecc1f763153bd07fb4428eab2700a6
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-05-01 18:35:44 +0200

    sass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import './css/main.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hei på deg film netside</h1>
     </div>
   );
 }
