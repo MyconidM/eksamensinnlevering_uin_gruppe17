45abad713501834b1afb2c811be4bde4c8eb08e7
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-05-12 21:41:27 +0200

    api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import './css/main.css'
+import Main from './components/Main';
+
 
 function App() {
+ 
   return (
     <div className="App">
       <h1>Hei på deg film netside</h1>
+      <Main />
     </div>
   );
 }
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,41 +1,24 @@
-import { useEffect, useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import MovieCard from "./MovieCard";
-import MovieCardHeading from "./MovieCardHeading";
-import SearchBox from "./SearchResults";
 
-// funksjonen er basert på  løsningen vis i https://www.youtube.com/watch?v=jc9_Bqzy2YQ
+// https://stackoverflow.com/questions/73761980/rawg-io-api-call-javascript-to-display-in-html
 export default function Main(){
+    async function fetchData() {
+        return (await fetch('https://api.rawg.io/api/games?key=834628e421154a1e8191857d89debae3')).json();
+      }
+      document.addEventListener("DOMContentLoaded", async () => {
+        let games = [];
+        try {
+            games = await fetchData();
+        } catch (e) {
+            console.log("Error!");
+            console.log(e);
+        }
+        console.log(games);
+      });
 
-    const [movies, setMovies] = useState([]);
-    const [movieInfo, setMovieInfo] = useState([]);
-    const [searchValue, setSearchValue] = useState ('James Bond')
-    
-    const getMovieRequest = async () => {
-        const url =`http://www.omdbapi.com/?s=${searchValue}&type=movie&apikey=70816349`
-
-        const response = await fetch(url).then(response => response.json());
-        setMovies(response.Search)
-        return response
-    }
-// løsningn for getMovieInfo fikk jeg hjelp av medstudenter og studass for å løse 
-    const getMovieInfo = async () => {
-        const { Search: search } = await getMovieRequest();
-        const movieInfoPromises = search.map(async movie => {
-          const url = `http://www.omdbapi.com/?i=${movie.imdbID}&type=movie&apikey=70816349`;
-          const response = await fetch(url);
-            return await response.json();
-        });
-        const movieInfo = await Promise.all(movieInfoPromises);
-        setMovieInfo(movieInfo);
-      };
+      window.onload = function appendData() {
+        console.log('The games will load now.');
+      }
 
 
-    useEffect(() => {
-        getMovieInfo()
-        console.log(movieInfo)
-    }, [searchValue])
-
-    
-
+      
 }
