3438062c15ff53098ed3a08c02ffaae47c989249
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-05-18 00:17:04 +0200

    Top 10 fø søk

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -6,10 +6,30 @@ import Favorites from './favorites';
 import Results from "./Results";
 
 export default function Main(){
-const [search, setSearch] = useState('last')
-
-const [gameInfo, setGameInfo] = useState([]);
+    const [search, setSearch] = useState('')
+    const [gameInfo, setGameInfo] = useState([]);
+    let slug = search.split(' ').join('-').toLowerCase()
       
+    async function searchGames() {
+      const apiKey = '834628e421154a1e8191857d89debae3'; 
+      const url = `https://api.rawg.io/api/games?key=${apiKey}&search=${slug}`;
+  
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+    
+        console.log(data.results);
+        setGameInfo(data.results); // Set the fetched data to gameInfo
+      } catch (error) {
+        console.log('Error:', error);
+      }
+    }
+    
+    useEffect(() => {
+        // Fetch top movies when the component mounts
+        searchGames();
+      }, []);  
+    
     async function fetchNewestGames() {
       const apiKey = '834628e421154a1e8191857d89debae3'; 
       const url = `https://api.rawg.io/api/games?key=${apiKey}&dates=2021-01-01,2023-05-16&ordering=-released&page_size=10`;
@@ -19,7 +39,7 @@ const [gameInfo, setGameInfo] = useState([]);
       const data = await response.json();
   
       
-      console.log(data.results);
+      console.log(data.results);    
       } catch (error) {
       console.log('Error:', error);
       }
@@ -34,7 +54,7 @@ const [gameInfo, setGameInfo] = useState([]);
     <Router>
       <div className='app'>
         <Nav />
-        <Search search={search} setSearch={setSearch}/>
+        <Search search={search} setSearch={setSearch} searchGames={searchGames}/>
         <Routes>
             <Route path="/" index element={Main} />
             <Route exact path='/favorites' Component={Favorites} />
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,32 +1,9 @@
 import { useEffect, useState } from "react"
 import Results from "./Results"
 
-export default function Search( item){
-  const [search, setSearch] = useState('')
-  const [gameInfo, setGameInfo] = useState([]);
-  let slug = search.split(' ').join('-').toLowerCase()
-    
-  async function searchGames() {
-    const apiKey = '834628e421154a1e8191857d89debae3'; 
-    const url = `https://api.rawg.io/api/games?key=${apiKey}&search=${slug}`;
-
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
+export default function Search({setSearch, search, searchGames}){
   
-      console.log(data.results);
-      setGameInfo(data.results); // Set the fetched data to gameInfo
-    } catch (error) {
-      console.log('Error:', error);
-    }
-  }
 
-useEffect(() => {
-  if (search.length >= 3) {
- searchGames();
-}
-
-}, [search]);
 
 return (
       <div className="col col-sm-4 serch">
@@ -36,6 +13,7 @@ return (
                     onChange={(event) => setSearch(event.target.value)}
                     placeholder="Søk på spill...">
                 </input>
+                <button type="submit" onClick={searchGames}>Search</button> 
             </div>
     );
 }
