cdbe3cf82edc73bcdb253551e44938ed5c6492ce
IvarTandberg <122887791+IvarTandberg@users.noreply.github.com>
2023-05-18 20:49:55 +0200

    link

--- frontend/src/components/Singlegame.js
+++ frontend/src/components/Singlegame.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 
-export default function Singlegame({gameInfo}) {
-    return (
-        <div key={game.id}>
-                <img src={game.background_image}></img>
-              <h3>{game.name}</h3>
-              <p>{game.description}</p>
-            </div>
-      );
-   }
+export default function Singlegame() {
+  const { id } = useParams();
+  const [game, setGame] = useState(null);
+
+  useEffect(() => {
+    // Fetch game details based on the ID
+    // Example code, replace it with your API call or data retrieval logic
+    fetch(`/api/games/${id}`)
+      .then((response) => response.json())
+      .then((data) => setGame(data));
+  }, [id]);
+
+  console.log(game)
+  return (
+    <div>
+      {game ? (
+        <div>
+          <h2>{game.name}</h2>
+          {game.genres.map(genre => (
+            <p>{genre.name}</p>
+          ))}
+          
+          {/* Additional content here */}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+
+
+
+
+
+
 
 
   
