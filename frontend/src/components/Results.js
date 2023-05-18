30615e10c593e877a55281c6e28691f113d216d2
IvarTandberg <122887791+IvarTandberg@users.noreply.github.com>
2023-05-18 14:25:27 +0200

    Gtuppe_info_Ivar

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -5,12 +5,15 @@ export default function Results({gameInfo}){
     return (
         <div>
           {gameInfo.map((game) => (
+            
             <div key={game.id}>
                 <img src={game.background_image}></img>
               <h3>{game.name}</h3>
               <p>{game.description}</p>
+              <Link to={game.slug} className="link"></Link>
             </div>
           ))}
         </div>
       );
 }
+
