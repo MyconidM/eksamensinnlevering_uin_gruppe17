d43a438042b749baae71b26253aa0b8f6c94f885
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-05-18 15:19:43 +0200

    favorits start

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Favorites from './favorites';
 
 export default function Results({gameInfo}){
     return (
@@ -9,6 +10,7 @@ export default function Results({gameInfo}){
                 <img src={game.background_image}></img>
               <h3>{game.name}</h3>
               <p>{game.description}</p>
+              <button onClick={<Favorites game={game}/>}>Add too favorits</button>
             </div>
           ))}
         </div>
