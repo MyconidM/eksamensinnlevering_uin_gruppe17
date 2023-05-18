afe0a4547d344f0c82cd795add965d2742542906
Johnny Inzagi <110547404+Spankoh@users.noreply.github.com>
2023-05-18 21:54:53 +0200

    css

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -5,12 +5,12 @@ import Singlegame from './Singlegame';
 
 export default function Results({gameInfo}){
     return (
-        <div>
+        <div className='game-package'>
           {gameInfo.map((game) => (
             
-            <div key={game.id}>
-                <img src={game.background_image}></img>
-              <Link to={'/Singlegame'}><h3>{game.name}</h3></Link>
+            <div className='game-item' key={game.id}>
+                <img className='game-posters' src={game.background_image}></img>
+              <Link to={'/Singlegame'}><h3 className='game-name'>{game.name}</h3></Link>
               <p>{game.description}</p>
               <button onClick={<Favorites game={game}/>}>Add too favorits</button>
             </div>
