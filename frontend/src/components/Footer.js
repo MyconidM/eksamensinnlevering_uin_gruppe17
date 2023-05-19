35d08ec8e59ed141930f3ac02003d648fe210c54
IvarTandberg <122887791+IvarTandberg@users.noreply.github.com>
2023-05-19 06:17:47 +0200

    css edit 06:17

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,10 @@
 export default function Footer () {
  return (
     <div className="footer">
-        <p>Credit too <a href="https://rawg.io/">RAWG.io</a></p>
+        <p>Credit to: <a href="https://rawg.io/">RAWG.io</a></p>
         <a href="https://rawg.io/apidocs"><p>apidocs</p></a>
         <a href="https://api.rawg.io/docs/">Documentation</a>
+        <a href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">Font</a>
     </div>
  )
 }
