0f27ffed40915202f74611b09b068926407863c2
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-05-19 07:30:00 +0200

    serch dashboard, favorites på dashboard

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -1,20 +1,20 @@
 import Main from "./Main";
 import MyGamesPage from "./MyGamesPage";
+import Favorites from "./favorites";
 import GameShop from "./gameshop";
 //https://stackoverflow.com/questions/57236204/how-to-return-only-3-elements-from-arrays-of-object-in-react
-export default function Dashboard ({gameInfo, genreInfo}) {
+export default function Dashboard ({gameInfo, genreInfo, search, searchGames, setSearch}) {
    
  return (
     <div>
       <div className="gameShop">
-         <GameShop gameInfo={gameInfo.slice(0, 3)} />
+         <GameShop gameInfo={gameInfo.slice(0, 3)} search={search} setSearch={setSearch} searchGames={searchGames}/>
       </div>
       <div className="MyGames">
          <MyGamesPage genreInfo={genreInfo.slice(0, 4)} />
       </div>
       <div className="favorite">
-         
-
+         <Favorites />
       </div>
     </div>
  )
