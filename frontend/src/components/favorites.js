93723a7eb208154c49d65b86c676f455eab09845
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-05-19 06:41:23 +0200

    favorites

--- frontend/src/components/favorites.js
+++ frontend/src/components/favorites.js
@@ -1,14 +1,36 @@
-import { useState } from "react";
-import Results from "./Results";
+import { useAppContext } from './appContext';
 
 //https://www.youtube.com/watch?v=oU2c_02YaME&t=2342s
 export default function Favorites(game) {
-    const [favorties, setFavorties] = useState([]);
+    const {favorties, addToFavorites, removeFromFavorites} = useAppContext();
+
+    console.log('Favorites are '+favorties)
+
+    const favoriteChecker = (id) => {
+        const boolean = favorties.some((game) => game.id === id);
+        return boolean
+    }
     return (
+
         
-        <>
+        <section>
         <h1>Favoritter</h1>
-        <Results favorties={favorties} />
-        </>
+        <div>
+        {favorties.length > 0 ? favorties.map((game) => (
+            
+            <div className='game-item' key={game.id}>
+                <img className='game-posters' src={game.background_image}></img>
+              <h3 className='game-name'>{game.name}</h3>
+              <div className='game-genre'>{game.genres.map(genre => (
+                <span>{genre.name}, </span>
+              ))}</div>
+              {favoriteChecker(game.id) ? 
+              <button className='button-favorite' onClick={() => removeFromFavorites(game.id)}>Remove from Favorite</button>
+            : <button className='button-favorite' onClick={() => addToFavorites(game)}>Add too Favorite</button>}
+              <button>Buy</button>
+            </div>
+          )) : <h2>You don't have any favorites yet!</h2>}
+        </div>
+        </section>
     )
    }
