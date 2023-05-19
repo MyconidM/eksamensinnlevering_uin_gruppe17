3dbf79acf9e2dabfeda95586096fb91971734f34
IvarTandberg <122887791+IvarTandberg@users.noreply.github.com>
2023-05-19 10:42:05 +0200

    Footer fiks

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer () {
  return (
     <div className="footer">
-        <p>Credit to: <a href="https://rawg.io/">RAWG.io</a></p>
-        <a href="https://rawg.io/apidocs"><p>apidocs</p></a>
-        <a href="https://api.rawg.io/docs/">Documentation</a>
-        <a href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">Font</a>
+        <p>Credit to: <a className="footeritem" href="https://rawg.io/">RAWG.io</a></p>
+        <a className="footeritem" href="https://rawg.io/apidocs"><p>apidocs</p></a>
+        <a className="footeritem" href="https://api.rawg.io/docs/"><p>Documentation</p></a>
+        <a className="footeritem" href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet"><p>Font</p></a>
     </div>
  )
 }
